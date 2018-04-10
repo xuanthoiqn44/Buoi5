0914f65cedad17a4ac6569280fc1d3abdbdc4332
Tran Xuan Thoi <35894580+xuanthoiqn44@users.noreply.github.com>
2018-04-10 22:38:07 +0700

    done

---
+++ Buoi2_client/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+
+namespace Buoi2_client
+{
+    public partial class FormClient : Form
+    {
+        delegate void MyDelegate(string mess);
+        byte[] data = new byte[1024];
+        string input, stringData;
+        IPEndPoint ipep;
+        Socket server;
+        Socket sock;
+        string mess = "Client: ";
+        public FormClient()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void buttonexit_Click(object sender, EventArgs e)
+        {
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+            this.Close();
+        }
+
+        private void buttonsend_Click(object sender, EventArgs e)
+        {
+            input =  textBox1.Text;
+            data = new byte[1024];
+            data = Encoding.ASCII.GetBytes(input);
+            textBox1.Text = "";
+            server.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendData), server);
+        }
+        private  void SendData(IAsyncResult iar)
+        {
+            Socket server = (Socket)iar.AsyncState;
+            int sent = server.EndSend(iar);
+            Invoke(new MethodInvoker(delegate { listBoxclient.Items.Add(input); }));
+            data = new byte[1024];
+            server.BeginReceive(data, 0, data.Length, SocketFlags.None, new AsyncCallback(ReceivedData), server);
+        }
+        void ReceivedData(IAsyncResult iar)
+        {
+            Socket remote = (Socket)iar.AsyncState;
+            int recv = remote.EndReceive(iar);
+            string receivedData = Encoding.ASCII.GetString(data, 0, recv);
+            Invoke(new MethodInvoker(delegate { listBoxclient.Items.Add(receivedData); }));
+        }
+
+
+
+        private void buttonketnoi_Click(object sender, EventArgs e)
+        {
+            ipep = new IPEndPoint(IPAddress.Parse(textBox2.Text), 9999);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+
+                server.BeginConnect(ipep, new AsyncCallback(Connected), server);
+                
+            }
+            catch (SocketException ex)
+            {
+                listBoxclient.Items.Add("Unable to connect to servers.");
+                listBoxclient.Items.Add(ex.ToString());
+                return;
+            }
+        }
+        public void Connected(IAsyncResult iar)
+        {
+            sock = (Socket)iar.AsyncState;
+            sock.EndConnect(iar);
+        }
+
+    }
+}
---
+++ Server/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+
+namespace Server
+{
+    public partial class FormServer : Form
+    {
+        IPEndPoint ipep;
+        Socket newsock;
+        Socket client;
+        Socket server;
+        IPEndPoint clientep;
+        byte[] data = new byte[1024];
+        string input;
+        delegate void MyDelegate(string mess);
+        string mess = "Server: ";
+        public FormServer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                ipep = new IPEndPoint(IPAddress.Any, 9999);
+                newsock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                newsock.Bind(ipep);
+                newsock.Listen(10);
+                listBoxserver.Items.Add("Waiting for a client...");
+                newsock.BeginAccept(new AsyncCallback(CallAccept), newsock);
+                
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            
+
+        }
+        public void CallAccept(IAsyncResult iar)
+        {
+             server = (Socket)iar.AsyncState;
+             client = server.EndAccept(iar);
+            data = new byte[1024];
+            client.BeginReceive(data, 0, data.Length, SocketFlags.None, new AsyncCallback(ReceivedData), client);
+        }
+
+
+        private void buttonsend_Click(object sender, EventArgs e)
+        {
+            input =  textBox1.Text;
+            listBoxserver.Items.Add(input);
+            textBox1.Text = "";
+            data = new byte[1024];
+            data = Encoding.ASCII.GetBytes(input );
+            client.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendData), client);
+        }
+        private void SendData(IAsyncResult iar)
+        {
+             server = (Socket)iar.AsyncState;
+            int sent = server.EndSend(iar);
+            Invoke(new MethodInvoker(delegate { listBoxserver.Items.Add(input); }));
+            client.BeginReceive(data, 0, data.Length, SocketFlags.None, new AsyncCallback(ReceivedData), client);
+        }
+        void ReceivedData(IAsyncResult iar)
+        {
+            Socket remote = (Socket)iar.AsyncState;
+            int recv = remote.EndReceive(iar);
+            data = new byte[1024];
+            string receivedData = Encoding.ASCII.GetString(data, 0, recv);
+            Invoke(new MethodInvoker(delegate{ listBoxserver.Items.Add(receivedData); }));
+        }
+
+        private void buttonexit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //public void addlistbox(string mess)
+        //{
+        //    listBoxserver.Items.Add(mess);
+        //}
+    }
+}
